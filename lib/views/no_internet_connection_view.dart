954c653a9c1f29ee253d3a7b9679e22dc5797d40
Reza Hartono <rezahartono11@gmail.com>
2022-06-29 20:35:00 +0700

    perbaikan no internet connection

--- lib/views/no_internet_connection_view.dart
+++ lib/views/no_internet_connection_view.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:weather/utils/size_config.dart';
 import 'package:weather/utils/theme.dart';
+import 'package:weather/views/home_page_view.dart';
 
 class NoInternetConnectionView extends StatelessWidget {
   const NoInternetConnectionView({Key? key}) : super(key: key);
@@ -9,48 +11,53 @@ class NoInternetConnectionView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: COLOR.PRIMARY,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            'No Internet Connection',
-            style: TEXT_STYLE.PRIMARY.copyWith(
-              fontSize: 24,
-              fontWeight: FONT_WEIGHT.BOLD,
+      body: SizedBox(
+        width: SIZE_CONFIG.SCREEN_WIDTH,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'No Internet Connection',
+              style: TEXT_STYLE.PRIMARY.copyWith(
+                fontSize: 24,
+                fontWeight: FONT_WEIGHT.BOLD,
+              ),
             ),
-          ),
-          SizedBox(
-            height: SIZE_CONFIG.DEFAULT_MARGIN,
-          ),
-          SizedBox(
-            width: SIZE_CONFIG.BLOCK_HORIZONTAL * 35,
-            height: SIZE_CONFIG.BLOCK_VERTICAL * 6,
-            child: TextButton.icon(
-              onPressed: () {},
-              style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.resolveWith(
-                  (states) => COLOR.BLUE,
-                ),
-                overlayColor: MaterialStateProperty.resolveWith(
-                  (states) => COLOR.DARK,
-                ),
-                shape: MaterialStateProperty.resolveWith(
-                  (states) => RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
+            SizedBox(
+              height: SIZE_CONFIG.DEFAULT_MARGIN,
+            ),
+            SizedBox(
+              width: SIZE_CONFIG.BLOCK_HORIZONTAL * 35,
+              height: SIZE_CONFIG.BLOCK_VERTICAL * 6,
+              child: TextButton.icon(
+                onPressed: () {
+                  Get.offAll(const HomePageView());
+                },
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.resolveWith(
+                    (states) => COLOR.BLUE,
+                  ),
+                  overlayColor: MaterialStateProperty.resolveWith(
+                    (states) => COLOR.DARK,
+                  ),
+                  shape: MaterialStateProperty.resolveWith(
+                    (states) => RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
                   ),
                 ),
+                icon: Icon(
+                  Icons.refresh,
+                  color: COLOR.WHITE,
+                ),
+                label: Text(
+                  'Refresh',
+                  style: TEXT_STYLE.PRIMARY,
+                ),
               ),
-              icon: Icon(
-                Icons.refresh,
-                color: COLOR.WHITE,
-              ),
-              label: Text(
-                'Refresh',
-                style: TEXT_STYLE.PRIMARY,
-              ),
-            ),
-          )
-        ],
+            )
+          ],
+        ),
       ),
     );
   }
